627a7babc7acfb93d15f72903802e15b40345b33
Sara Reed <reedsara90@gmail.com>
2021-05-20 14:40:42 -0600

    set up, css started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import face from '../src/assets/face.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="red-line"></div>
+      <div id="blue-line"></div>
+      <div id="green-line"></div>
+      <div id="yellow-line"></div>
+      <img alt="" id="face-img" src={face} />
+      <h1> Sara Reed </h1>
+      <p> Web Developer</p>
+      <div id="red-section"></div>
+      <div id="red-mini-section"></div>
+
+      <div id="blue-section"></div>
+      <div id="green-section"></div>
+      <div id="yellow-section"></div>
+
     </div>
   );
 }
